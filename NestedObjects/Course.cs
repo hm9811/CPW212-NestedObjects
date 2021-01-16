1cd4608706cc5006b814d10a480f0acd867f9603
hm9811 <hm981198@gmail.com>
2021-01-16 03:34:10 -0800

    Added course class

---
+++ NestedObjects/Course.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    class Course
+    {
+        /// <summary>
+        /// The title of the course.
+        /// Ex : CPW 212 - ...
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The course catalog official description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// the number of college credits awarded...
+        /// </summary>
+        public byte Credits { get; set; }
+
+        /// <summary>
+        /// The instructor that teaches the course
+        /// </summary>
+        public Instructor CourseInstructor { get; set; }
+
+        /// <summary>
+        /// All the students currently enrooled in the class
+        /// </summary>
+        public List<Student> Roster { get; set; }
+    }
+}
